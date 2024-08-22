8056cd524a9f2779c9c6c5b7fc5ac4bdf50708a7
adhcode <149141211+adhcode@users.noreply.github.com>
2024-08-22 09:57:19 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -36,11 +36,28 @@ function page() {
     </div>
     
     <div className="flex flex-col justify-center items-center p-2 w-full">
-   <h1 className="mt-4 text-[#FF7F00] font-bold text-[20px]">Yanga By MeekTurna</h1>
-   <p className="text-[15px]">Choose your preferred music service</p>
+   <h1 className="mt-4 text-[#2A2A2A] font-bold text-[20px]">Yanga By MeekTurna</h1>
+   <p className="text-[25px]">OUT NOW</p>
    </div>
-   <Separator className="my-4 bg-[#FF7F00] opacity-10" />
+   <Separator className="my-4 bg-[#0443E2] opacity-20" />
    <div className="flex justify-center items-center gap-[120px] pl-[20px] pr-[20px]">
+      <div >
+       <Image src="/apple.png"
+       
+       width={100}
+      height={100}
+      alt="uvise logo"
+      className="object-contain"/>
+      </div>
+      <div>
+        <Link href="https://music.apple.com/us/album/yanga-single/1760751055">
+      <Button className=" border-[#0443E2] border-[1px]">Play</Button>
+      </Link>
+      </div>
+      
+    </div>
+    <Separator className="my-4 bg-[#0443E2] opacity-20" />
+    <div className="flex justify-center items-center gap-[120px] pl-[20px] pr-[20px]">
       <div >
        <Image src="/spotify.png"
        
@@ -50,13 +67,101 @@ function page() {
       className="object-contain"/>
       </div>
       <div>
-        <Link href="https://distrokid.com/hyperfollow/meekturna/yanga">
-      <Button className=" border-[#FF7F00] border-[1px]">Pre Save</Button>
+        <Link href="https://open.spotify.com/album/14ftgJ4eRqUdLkGD9N0Y11?si=QE1wWtciQ-iMWH5z4XUhGQ">
+      <Button className=" border-[#0443E2] border-[1px]">Play</Button>
       </Link>
       </div>
       
     </div>
-    <Separator className="my-4 bg-[#FF7F00] opacity-5" />
+    <Separator className="my-4 bg-[#0443E2] opacity-20" />
+    <div className="flex justify-center items-center gap-[120px] pl-[20px] pr-[20px]">
+      <div >
+       <Image src="/audiomack.png"
+       
+       width={100}
+      height={100}
+      alt="uvise logo"
+      className="object-contain"/>
+      </div>
+      <div>
+        <Link href="https://audiomack.com/meekturna/song/meekturna-yanga">
+      <Button className=" border-[#0443E2] border-[1px]">Play</Button>
+      </Link>
+      </div>
+      
+    </div>
+    <Separator className="my-4 bg-[#0443E2] opacity-20" />
+    <div className="flex justify-center items-center gap-[120px] pl-[20px] pr-[20px]">
+      <div >
+       <Image src="/boomplay.png"
+       
+       width={100}
+      height={100}
+      alt="uvise logo"
+      className="object-contain"/>
+      </div>
+      <div>
+        <Link href="https://www.boomplay.com/songs/176640830?srModel=COPYLINK&srList=WEB">
+      <Button className=" border-[#0443E2] border-[1px]">Play</Button>
+      </Link>
+      </div>
+      
+    </div>
+
+    <Separator className="my-4 bg-[#0443E2] opacity-20" />
+    <div className="flex justify-center items-center gap-[120px] pl-[20px] pr-[20px]">
+      <div >
+       <Image src="/deezer.png"
+       
+       width={100}
+      height={100}
+      alt="uvise logo"
+      className="object-contain"/>
+      </div>
+      <div>
+        <Link href="https://www.deezer.com/us/album/623886301">
+      <Button className=" border-[#0443E2] border-[1px]">Play</Button>
+      </Link>
+      </div>
+      
+    </div>
+
+    <Separator className="my-4 bg-[#0443E2] opacity-20" />
+    <div className="flex justify-center items-center gap-[120px] pl-[20px] pr-[20px]">
+      <div >
+       <Image src="/youtubemusic.png"
+       
+       width={100}
+      height={100}
+      alt="uvise logo"
+      className="object-contain"/>
+      </div>
+      <div>
+        <Link href="https://music.youtube.com/watch?v=TnEKQovkdsA&si=idUZxYxL4F-Xkg4u">
+      <Button className=" border-[#0443E2] border-[1px]">Play</Button>
+      </Link>
+      </div>
+      
+    </div>
+   
+
+    <Separator className="my-4 bg-[#0443E2] opacity-20" />
+    <div className="flex justify-center items-center gap-[120px] pl-[20px] pr-[20px]">
+      <div >
+       <Image src="/tidal.png"
+       
+       width={100}
+      height={100}
+      alt="uvise logo"
+      className="object-contain"/>
+      </div>
+      <div>
+        <Link href="https://open.spotify.com/album/14ftgJ4eRqUdLkGD9N0Y11?si=QE1wWtciQ-iMWH5z4XUhGQ">
+      <Button className=" border-[#0443E2] border-[1px]">Play</Button>
+      </Link>
+      </div>
+      
+    </div>
    </div>
 
    
